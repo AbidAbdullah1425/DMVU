812c7eef3876d3fa4997661e8ee0a82a389fa718
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-01-31 12:22:15 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,67 +1,19 @@
-#(©)CodeXBotz
-
-
-
 
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-
-
-#Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7438884533:AAGUCrHeOxpBJrXXu3PTyrkWPirgYkwbuIc")
-
-#Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "26254064"))
-
-#Your API Hash from my.telegram.org
+API_ID = int(os.environ.get("API_ID", "26254064"))
 API_HASH = os.environ.get("API_HASH", "72541d6610ae7730e6135af9423b319c")
-
-#Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002191732189"))
-
-#OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5296584067"))
+BOT_TOKEN = '0'
 
-#Port
-PORT = os.environ.get("PORT", "8080")
+# Dailymotion API credentials
+CLIENT_ID = '8fc35d2179736e12a797'
+CLIENT_SECRET = '3f999282f562df4d2e890ee38a7bc659c5c1edf1'
+ACCESS_TOKEN = 'NnJXdFQ9MGEAMA0bXA5aET4PFBccfB03Lz8RFAEMLiI4'
+REFRESH_TOKEN = 'YOUR_DAILYMOTION_REFRESH_TOKEN'
 
-#Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://teamprosperpay:AbidAbdullah199@cluster0.z93fita.mongodb.net/")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
-
-#force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1002077054432"))
-FORCE_SUB_CHANNEL_2 = int(os.environ.get("FORCE_SUB_CHANNEL_2", "-1002003740934"))
-FORCE_SUB_CHANNEL_3 = int(os.environ.get("FORCE_SUB_CHANNEL_3", "-1002125561929"))
-FORCE_SUB_CHANNEL_4 = int(os.environ.get("FORCE_SUB_CHANNEL_4", "-1002092136573"))
-
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
-
-#start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first} I'm a bot who can store files and share it via spacial links")
-try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5296584067").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-
-#Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "You have to join our Channels First")
-
-#set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
-
-#set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
-
-#Set true if you want Disable your Channel Posts Share button
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
-
-BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(5296584067)
