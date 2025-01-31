32149cb0422265e3387424b26a028c82ffae7ddb
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-01-31 13:07:11 +0600

    Update config.py

--- config.py
+++ config.py
@@ -16,10 +16,6 @@ CLIENT_SECRET = os.environ.get("CLIENT_SECRET", '3f999282f562df4d2e890ee38a7bc65
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN", "czhIS2EYN3p5Hh1RRypnOhkKXwBNREQ4EA0kTFYhIiEO")  # New Access Token
 REFRESH_TOKEN = os.environ.get("REFRESH_TOKEN", "274f209725e532615d9c77c933614b2633243257")  # New Refresh Token
 
-
-ADMINS.append(OWNER_ID)
-ADMINS.append(5296584067)
-
 LOG_FILE_NAME = "filesharingbot.txt"
 
 logging.basicConfig(
